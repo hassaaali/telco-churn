f255a2b2a43bec82176d42b35d24c4b191652ae2
hassaaali <hassaaali@tesla.com>
2023-10-18 23:47:25 -0700

    frontend HTML code

---
+++ gui/README.md
@@ -0,0 +1,2 @@
+# telco-churn
+An AI/ML program to predict the telco churn of a customer i.e. will the customer unsubscribe or not?
---
+++ gui/main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_session import Session  # https://pythonhosted.org/Flask-Session
+
+app = Flask(__name__)
+Session(app)
+
+@app.route('/telco_churn', methods=['GET', 'PUT'])
+
+def telco_churn():
+    try:
+        return render_template('base.html')
+    except Exception as e:
+        return render_template('error.html', error=e)
+
+if __name__ == '__main__':
+        app.run(host="0.0.0.0", port=8000)
